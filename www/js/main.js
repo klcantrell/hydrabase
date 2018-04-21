bd85b2548983a2adf460280b4142d89613a1aebb
Kalalau <cantrellkalalau@gmail.com>
2018-04-21 09:14:50 -0400

    playing with geolocation

---
+++ www/js/main.js
@@ -0,0 +1,21 @@
+if (navigator.geolocation) {
+  navigator.geolocation.getCurrentPosition(
+    handleLocationFound,
+    handleLocationError
+  );
+} else {
+  handleLocationError(err);
+}
+
+function handleLocationFound(position) {
+  const pos = {
+    lat: position.coords.latitude,
+    lng: position.coords.longitude
+  };
+
+  console.log(pos);
+}
+
+function handleLocationError(err) {
+  console.log(err);
+}
